277468ebae6771aa79ccf0c9156799ca9d62d6d2
Dabiidi <daveleonardengracia@gmail.com>
2023-10-16 14:37:10 +0800

    fix/ ios feature

--- Screens/Notifications/Notification.screen.tsx
+++ Screens/Notifications/Notification.screen.tsx
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import { useGetItems } from "../../services/ItemsAPI";
+import {
+  Container,
 
-const NotifScreen= () => {
+} from "./NotificatiionStyle";
+import { useNavigation } from "@react-navigation/native";
+import InventoryComponent from "../Inventory/Inventory";
+const NotificationScreen = () => {
+  const navigation = useNavigation();
+  const [lowStockCount, setLowStockCount] = React.useState();
+  const { data, isLoading } = useGetItems();
+
+
+  const navigateToScreen = (inventory: any) => {
+    navigation.navigate("Main", {
+      screen: "Inventory Detail",
+
+      params: { inventory },
+    });
+  };
+
+  React.useEffect(() => {
+    const filteredLowStockQuantity = data.filter(
+      (item: any) => item.quantity < 5
+    );
+    setLowStockCount(filteredLowStockQuantity);
+  }, [data]);
+
   return (
-    <View>
-      <Text>Notification.screen</Text>
-    </View>
-  )
-}
+    <>
+      <Container>
+        <FlatList
+          data={lowStockCount}
+          renderItem={({ item }) => (
+            <InventoryComponent
+            onPress={() => navigateToScreen(item)}
+            item={item}
+          />
+          )}
+          scrollEnabled
+          keyExtractor={(item, index) => item.id || index.toString()}
+        />
+      </Container>
+    </>
+  );
+};
 
-export default NotifScreen 
+export default NotificationScreen;
